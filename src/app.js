422d9ace0026213a80a58bc415d05e6569b6cecb
Marton <mdox@mdox.xyz>
2023-01-02 21:28:25 +0100

    Typo

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const { STATIC_DIR } = require("./consts");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.use("/download", express.static(STATIC_DIR));
 
 Object.assign(exports, { app });
